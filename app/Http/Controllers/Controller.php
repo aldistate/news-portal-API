9d014669faef1af4a8fc3683f88d36e3363d9f89
Irfan Aditya <helloirfanaditya@gmail.com>
2023-04-13 22:08:00 +0700

    refactor post controller using helper handler

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,22 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    const RESPONSE_SUCCESS = true;
+    const RESPONSE_ERROR = false;
+
+    protected function jsonRes($message, $status = self::RESPONSE_ERROR, $responseCode = 400)
+    {
+
+        if ($status == self::RESPONSE_SUCCESS) {
+            $responseCode = 200;
+            $data = $message;
+        }
+        $response = [
+            "code" => (string) $responseCode,
+            "status" => $status,
+            "data" => $message,
+        ];
+        return response()->json($response, $responseCode);
+    }
 }
--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,16 +12,33 @@ class PostController extends Controller
     public function index()
     {
         $posts = Post::all();
-        // return response()
-        //         ->json(['data' => $posts]);
-        return PostResource::collection($posts);
+        $results = [];
+
+        foreach($posts as $post) {
+            $results[] = [
+                'id' => $post->id,
+                'title' => $post->title,
+                'news_content' => $post->news_content,
+                'created_at' => date_format($post->created_at, 'Y-m-d H:i:s'),
+            ];
+        }
+        return $this->jsonRes($results,self::RESPONSE_SUCCESS);
     }
 
     public function show($id)
     {
-        $post = Post::findOrFail($id);
-        // return response()
-        //         ->json(['data' => $post]);
-        return new PostDetailResource($post);
+        $post = Post::whereId($id)->first();
+
+        if(is_null($post)) return $this->jsonRes(['message' => 'No row found'],self::RESPONSE_ERROR,404);
+
+        $result = [
+            'id' => $post->id,
+            'author' => 'ditulis oleh ' . $post->user->username,
+            'title' => $post->title,
+            'news_content' => $post->news_content,
+            'created_at' => date_format($post->created_at, 'Y-m-d H:i:s'),
+        ];
+
+        return $this->jsonRes($result,self::RESPONSE_SUCCESS);
     }
 }
